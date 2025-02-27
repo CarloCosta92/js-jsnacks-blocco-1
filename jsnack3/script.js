06ba79783bb8f238c10046115fef8ebde70157df
Carlo Costa <carlo.costa92@gmail.com>
2025-02-27 12:38:30 +0100

    feat: solved third exercise

---
+++ jsnack3/script.js
@@ -0,0 +1,19 @@
+//jsnack3
+
+//Il software deve chiedere per 10 volte all’utente di inserire un numero.
+
+//Il programma stampa la somma di tutti i numeri inseriti.
+
+let num1= parseInt(prompt("inserisci un numero"));
+let num2= parseInt(prompt("inserisci un numero"));
+let num3= parseInt(prompt("inserisci un numero"));
+let num4= parseInt(prompt("inserisci un numero"));
+let num5= parseInt(prompt("inserisci un numero"));
+let num6= parseInt(prompt("inserisci un numero"));
+let num7= parseInt(prompt("inserisci un numero"));
+let num8= parseInt(prompt("inserisci un numero"));
+let num9= parseInt(prompt("inserisci un numero"));
+let num10= parseInt(prompt("inserisci un numero"));
+
+let sum = num1+num2+num3+num4+num5+num6+num7+num8+num9+num10 ;
+console.log(sum);
