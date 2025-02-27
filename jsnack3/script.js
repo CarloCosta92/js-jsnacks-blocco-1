88765f46316a409d47741dd13d4b05e0565b6f11
Carlo Costa <carlo.costa92@gmail.com>
2025-02-27 17:38:32 +0100

    feat: add new solution

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -4,16 +4,28 @@
 
 //Il programma stampa la somma di tutti i numeri inseriti.
 
-let num1= parseInt(prompt("inserisci un numero"));
-let num2= parseInt(prompt("inserisci un numero"));
-let num3= parseInt(prompt("inserisci un numero"));
-let num4= parseInt(prompt("inserisci un numero"));
-let num5= parseInt(prompt("inserisci un numero"));
-let num6= parseInt(prompt("inserisci un numero"));
-let num7= parseInt(prompt("inserisci un numero"));
-let num8= parseInt(prompt("inserisci un numero"));
-let num9= parseInt(prompt("inserisci un numero"));
-let num10= parseInt(prompt("inserisci un numero"));
+// let num1= parseInt(prompt("inserisci un numero"));
+// let num2= parseInt(prompt("inserisci un numero"));
+// let num3= parseInt(prompt("inserisci un numero"));
+// let num4= parseInt(prompt("inserisci un numero"));
+// let num5= parseInt(prompt("inserisci un numero"));
+// let num6= parseInt(prompt("inserisci un numero"));
+// let num7= parseInt(prompt("inserisci un numero"));
+// let num8= parseInt(prompt("inserisci un numero"));
+// let num9= parseInt(prompt("inserisci un numero"));
+// let num10= parseInt(prompt("inserisci un numero"));
 
-let sum = num1+num2+num3+num4+num5+num6+num7+num8+num9+num10 ;
+// let sum = num1+num2+num3+num4+num5+num6+num7+num8+num9+num10 ;
+// console.log(sum);
+
+
+// seconda soluzione
+
+let sum = 0;
+
+for (let i = 0; i < 10; i++) {
+  let numero = parseInt(prompt("Inserisci un numero:"));
+  sum += numero;
+}
+
 console.log(sum);
--- jsnack4/script.js
+++ jsnack4/script.js
@@ -5,34 +5,51 @@
 
 //se è dispari inseriscilo nell’array
 
-let numero1 = parseFloat(prompt("Inserisci un numero:"));
-let numero2 = parseFloat(prompt("Inserisci un numero:"));
-let numero3 = parseFloat(prompt("Inserisci un numero:"));
-let numero4 = parseFloat(prompt("Inserisci un numero:"));
-let numero5 = parseFloat(prompt("Inserisci un numero:"));
-let numero6 = parseFloat(prompt("Inserisci un numero:"));
+//prima soluzione
+
+// let numero1 = parseFloat(prompt("Inserisci un numero:"));
+// let numero2 = parseFloat(prompt("Inserisci un numero:"));
+// let numero3 = parseFloat(prompt("Inserisci un numero:"));
+// let numero4 = parseFloat(prompt("Inserisci un numero:"));
+// let numero5 = parseFloat(prompt("Inserisci un numero:"));
+// let numero6 = parseFloat(prompt("Inserisci un numero:"));
+
+// let numeriDispari = [];
+
+// if (numero1 % 2 !== 0) {
+//     numeriDispari.push(numero1);
+// }
+// if (numero2 % 2 !== 0) {
+//     numeriDispari.push(numero2);
+// }
+// if (numero3 % 2 !== 0) {
+//     numeriDispari.push(numero3);
+// }
+// if (numero4 % 2 !== 0) {
+//     numeriDispari.push(numero4);
+// }
+// if (numero5 % 2 !== 0) {
+//     numeriDispari.push(numero5);
+// }
+// if (numero6 % 2 !== 0) {
+//     numeriDispari.push(numero6);
+// }
+
+// console.log("Numeri dispari inseriti:", numeriDispari);
+
+
+//seconda soluzione
 
 let numeriDispari = [];
 
-if (numero1 % 2 !== 0) {
-    numeriDispari.push(numero1);
-}
-if (numero2 % 2 !== 0) {
-    numeriDispari.push(numero2);
-}
-if (numero3 % 2 !== 0) {
-    numeriDispari.push(numero3);
-}
-if (numero4 % 2 !== 0) {
-    numeriDispari.push(numero4);
-}
-if (numero5 % 2 !== 0) {
-    numeriDispari.push(numero5);
-}
-if (numero6 % 2 !== 0) {
-    numeriDispari.push(numero6);
+for (let i = 0; i < 6; i++) {
+  let numero = parseInt(prompt("Inserisci un numero:"));
+  
+
+  if (numero % 2 !== 0) {
+    numeriDispari.push(numero);  
+  }
 }
 
-console.log("Numeri dispari inseriti:", numeriDispari);
-
+console.log(numeriDispari);
 
